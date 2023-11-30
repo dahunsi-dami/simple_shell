2ab6ee355b98534cd19617a41c1d09801b80fbc9
Dre <gbemigadare@gmail.com>
2023-11-30 03:26:09 -0800

    Write really pretty code that passes Betty's checks.

---
+++ mdsh.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - a function that prints a dollar sign.
+ * @ac: the number of arguments passed.
+ * @av: a 1D array of arguments passed.
+ *
+ * Return: always success (0).
+ */
+int main(int ac, char **av[])
+{
+	(void)ac, (void)av;
+
+	printf("$ ");
+
+	return (0);
+}
