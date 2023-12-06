b27dca5a900bedb46d86992886d52b0b726a07fe
Dre <gbemigadare@gmail.com>
2023-12-06 03:12:30 -0800

    Put all code in infinite while loop so shell doesn't exit.

--- _mdsh.c
+++ _mdsh.c
@@ -1,16 +1,27 @@
 #include "main.h"
 /**
- * main - a function that prints a dollar sign.
+ * main - prompts user & executes command inputted.
  * @ac: the number of arguments passed.
  * @av: a 1D array of arguments passed.
  *
- * Return: always success (0).
+ * Return: output of command executed or 0 for success.
  */
 int main(int ac, char *av[])
 {
+	char *buf;
+	ssize_t nchar;
+	size_t n;
+
 	(void)ac, (void)av;
 
-	_write("$ \n");
+	while (1)
+	{
+		_write("micdre:~$ ");
+
+		nchar = getline(&buf, &n, stdin);
+
+		_write(buf);
+	}
 
 	return (0);
 }
