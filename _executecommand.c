1bb3f2c4791aa0e07c7478ee6e6b5d88be39a082
mikoiyke <mikoiyke@gmail.com>
2023-12-13 05:10:39 -0800

    function to execute command created

---
+++ _executecommand.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * execute_command - executes a command using fork and execve.
+ * @hargv: array of arguments for the command.
+ * @envp: environment variables.
+ *
+ * Return: the path of the executed command.
+ */
+char *execute_command(char **hargv, char *envp[]) {
+	char *path;
+	int fd;
+
+	path = _checkcmdexists(hargv, envp);
+
+	fd = fork();
+
+	if (fd == -1) {
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+
+	if (fd == 0) {
+		if (execve(path, hargv, envp) == -1)
+			perror(hargv[0]);
+	}
+
+	wait(NULL);
+	return (path);
+}
