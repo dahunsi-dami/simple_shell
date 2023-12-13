d2cde6351a9f46b4a02e5d5f10bfaa11b53091a1
mikoiyke <mikoiyke@gmail.com>
2023-12-13 05:20:24 -0800

    prototypes in main.h

--- main.h
+++ main.h
@@ -9,9 +9,11 @@ void _write(char *s);
 void _prompt_user(void);
 ssize_t _readinput(int term, char **buf, size_t *n);
 void _exitshell(char *buf);
+void _freememory(char *buf, char *bufc, char **hargv, char *path);
 int _putchar(char c);
 int _strcmp(char *s1, char *s2);
 char *_strcpy(char *dest, char *src);
+char *_executecommand(char **hargv, char *envp[]);
 int main(int ac, char *av[]);
 
 #endif
