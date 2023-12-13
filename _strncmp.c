b57ad3ecf9e276e9ed7f7b4065613363933ddb92
Dre <gbemigadare@gmail.com>
2023-12-13 10:32:08 -0800

    Fix syntax and control flow of _strncmp.c.

--- _strncmp.c
+++ _strncmp.c
@@ -13,12 +13,12 @@ int _strncmp(const char *str1, const char *str2, size_t n)
 	size_t i;
 
 	for (i = 0; i < n; i++)
+	{
 		if (str1[i] != str2[i])
-		{
-		return (str1[i] - str2[i]);
-		}
-	if (str1[i] == '\0' || str2[i] == '\0')
-		break;
+			return (str1[i] - str2[i]);
+		if (str1[i] == '\0' || str2[i] == '\0')
+			break;
+	}
 
 	return (0);
 }
