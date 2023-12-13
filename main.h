d88de3cf1ad3bad5491ae2405df8f2fac0e96819
Dre <gbemigadare@gmail.com>
2023-12-13 04:08:32 -0800

    Add prototypes for new _promptuser, _readinput, and _exitshell functions.

--- main.h
+++ main.h
@@ -6,6 +6,9 @@
 #include <stdlib.h>
 
 void _write(char *s);
+void _prompt_user(void);
+ssize_t _readinput(int term, char **buf, size_t *n);
+void _exitshell(char *buf);
 int _putchar(char c);
 int _strcmp(char *s1, char *s2);
 char *_strcpy(char *dest, char *src);
