a385a24d5bdb1afa1274b64dd24372e657c44bd1
mikoiyke <mikoiyke@gmail.com>
2023-12-13 05:13:15 -0800

    betty errors for _executecommand.c fixed

--- _executecommand.c
+++ _executecommand.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * execute_command - executes a command using fork and execve.
  * @hargv: array of arguments for the command.
@@ -6,7 +7,8 @@
  *
  * Return: the path of the executed command.
  */
-char *execute_command(char **hargv, char *envp[]) {
+char *execute_command(char **hargv, char *envp[])
+{
 	char *path;
 	int fd;
 
@@ -14,12 +16,14 @@ char *execute_command(char **hargv, char *envp[]) {
 
 	fd = fork();
 
-	if (fd == -1) {
+	if (fd == -1)
+	{
 		perror("fork");
 		exit(EXIT_FAILURE);
 	}
 
-	if (fd == 0) {
+	if (fd == 0)
+	{
 		if (execve(path, hargv, envp) == -1)
 			perror(hargv[0]);
 	}
