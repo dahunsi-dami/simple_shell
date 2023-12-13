fb0db7003806266b37430e3433331f1a9ed41282
mikoiyke <mikoiyke@gmail.com>
2023-12-13 05:07:47 -0800

    funtion required to free memory

---
+++ _freememory.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _freememory - frees allocated memory for various variables.
+ * @buf: input buffer
+ * @bufc: copied input buffer
+ * @hargv: array of command arguments
+ * @path: path of the command
+ */
+
+void _freememory(char *buf, char *bufc, char **hargv, char *path)
+{
+	int j;
+
+	free(buf);
+	free(bufc);
+
+	if (hargv != NULL)
+	{
+		for (j = 0; hargv[j]; j++)
+			free(hargvp[j];
+					free(hargv);
+					}
+
+					if (path != NULL)
+					free(path);
+}
