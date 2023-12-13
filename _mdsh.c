f9ba7c67ebaadf0ee974a1d66a7fecdc983a5dab
Dre <gbemigadare@gmail.com>
2023-12-13 09:52:13 -0800

    Remove empty lines in code as much as possible.

--- _mdsh.c
+++ _mdsh.c
@@ -28,7 +28,6 @@ int main(int argc, char *argv[], char *envp[])
 		path = NULL;
 
 		nchar = _readinput(term, &buf, &n);
-
 		if (nchar == -1)
 		{
 			if (feof(stdin))
@@ -42,24 +41,19 @@ int main(int argc, char *argv[], char *envp[])
 				exit(EXIT_FAILURE);
 			}
 		}
-
 		if (_strcmp(buf, "exit\n") == 0)
 		{
 			free(buf, bufc, NULL, NULL);
 			exit(EXIT_SUCCESS);
 		}
-
 		bufc = malloc(sizeof(char) * (nchar + 1));
-
 		if (bufc == NULL)
 		{
 			free(buf, bufc, NULL, NULL);
 			perror("Buffer copying failed");
 			continue;
 		}
-
 		_strcpy(bufc, buf);
-
 		char **hargv = _tokenizeinput(buf, delim);
 
 		if (hargv == NULL)
@@ -68,9 +62,7 @@ int main(int argc, char *argv[], char *envp[])
 			perror("Tokenization failed");
 			continue;
 		}
-
 		path = _excute_command(hargv, envp);
-
 		_freememory(buf, bufc, hargv, path);
 	}
 	return (0);
