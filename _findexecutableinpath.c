52af78c6ba60606e56174023dcd830b215651e0f
mikoiyke <mikoiyke@gmail.com>
2023-12-13 08:29:37 -0800

    Create helper to find executable in directories of PATH variable.

---
+++ _findexecutableinpath.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _findexecutableinpath - finds executable in directories of PATH variable.
+ * @directories: array containing directories of PATH variable.
+ * @filename: name of the executable.
+ *
+ * Return: returns the path or NULL.
+ */
+char *_findexecutableinpath(char *directories[], const char *filename)
+{
+	char *path = NULL;
+	int i;
+
+	for (i = 0; directories[i] != NULL; i++)
+	{
+		path = _constructfullpath(directories[i], filename);
+
+		if (access(path, F_OK) == 0)
+			return (path);
+		free(path);
+		path = NULL;
+	}
+	return (NULL);
+}
